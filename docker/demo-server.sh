b343a8b1ca4792ab988b5316e18837ee8e397b3f
Scott Burch <scott@Scotts-MacBook-Pro-2.local>
2024-09-22 22:06:29 -0700

    update demo server startup script

--- docker/demo-server.sh
+++ docker/demo-server.sh
@@ -1,3 +1,4 @@
 docker compose -f compose-demo-server.yaml build --build-arg ARCH=amd64 --no-cache --parallel=1 && \
 
+docker compose -f compose-demo-server.yaml down && \
 docker compose -f compose-demo-server.yaml up -d
